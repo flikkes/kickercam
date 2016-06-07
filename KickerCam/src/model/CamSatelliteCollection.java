b4931f4763cf560e922fad7d483637359dc3ae8b
flikkes <lpke.flx@gmail.com>
2016-06-07 11:13:47 +0200

    corrected prev() method

--- KickerCam/src/model/CamSatelliteCollection.java
+++ KickerCam/src/model/CamSatelliteCollection.java
@@ -12,34 +12,47 @@ import java.util.List;
  * Stores a list of {@link CamSatellite Cam Satellites}
  */
 public class CamSatelliteCollection {
+
     private List<CamSatellite> camSatellites = new ArrayList<>();
     private int nextIndex = 0;
-    
+
     public void addCamSatellite(CamSatellite sat) {
         this.camSatellites.add(sat);
     }
-    
+
     public CamSatellite next() {
         CamSatellite returnValue = this.camSatellites.get(this.nextIndex);
-        
-        if (this.nextIndex == (this.camSatellites.size()-1)) {
+
+        if (this.nextIndex == (this.camSatellites.size() - 1)) {
             this.nextIndex = 0;
         } else {
             this.nextIndex++;
         }
-        
+
         return returnValue;
     }
-    
+
     public CamSatellite prev() {
-        CamSatellite returnValue = this.camSatellites.get(this.nextIndex);
+        int prevIndex;
         
-        if (this.nextIndex == (this.camSatellites.size()-1)) {
-            this.nextIndex = 0;
-        } else {
-            this.nextIndex++;
+        if (this.camSatellites.size() < 2) {
+            return this.next();
         }
         
+        if (this.nextIndex - 2 < 0) {
+            prevIndex = this.camSatellites.size() - 2;
+        } else {
+            prevIndex = this.nextIndex - 2;
+        }
+
+        if (this.nextIndex == 0) {
+            this.nextIndex = this.camSatellites.size()-1;
+        } else {
+            this.nextIndex--;
+        }
+        
+        CamSatellite returnValue = this.camSatellites.get(prevIndex);
+
         return returnValue;
     }
 }
